2d6d9fc9fb17e4eac6102270b4c2fd305aeb6293
Yasin <yasincoban1998@outlook.com>
2022-12-13 20:43:44 +0300

    aritmetik Operatörler

---
+++ lesson-12/aritmetikOp.rb
@@ -0,0 +1,60 @@
+a = 6
+b = 2
+
+# + : Toplama Operatörü
+
+puts a + b
+puts "Merhaba " + " Yasin"
+
+# - : Çıkarma Operatörü
+
+puts a - b
+
+#  : * Çarpma Operatörü
+
+puts a * b
+
+# / : Bölme Operatörü
+
+puts a / b
+puts 2/2,0
+
+# .div() : Tamsayı bölme Operatörü
+
+puts 5.div(2)
+
+# .fdiv() : Ondalık sayı bölme operatörü
+
+puts 5.fdiv(2)
+
+# -- ** : Üs Alma Operatörü
+
+puts a**2
+
+# % : Mod Alma Operatörü
+
+puts a % b
+
+# .modulo() : Mod alma
+
+puts a.modulo(b)
+
+# .remainder() : bölme işleminden kalanı verir
+
+puts a.remainder(b)
+
+# .divmod() : bölüm ve kalanı array olarak verir
+
+puts "[Bölüm, Kalan] - #{a.divmod(b)}"
+
+# .quo() : Bölümü  verir
+
+puts a.quo(b)
+
+
+
+
+
+
+
+
