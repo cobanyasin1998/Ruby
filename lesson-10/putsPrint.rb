b2ad651cc2b18e0375800e4f3b4b257695082471
Yasin <yasincoban1998@outlook.com>
2022-12-13 18:29:18 +0300

    puts print

---
+++ lesson-10/putsPrint.rb
@@ -0,0 +1,14 @@
+puts "1. çıktı: Bu değer konsola yazdırılacak"
+puts "2. çıktı: Bu değer konsola yazdırılacak"
+
+print "3. çıktı: Bu değer konsola yazdırılacak"
+print "4. çıktı: Bu değer konsola yazdırılacak"
+
+
+puts "1. Çıktı", "Bu değer", "konsola","yazdırılacak"
+print "1. Çıktı", "Bu değer", "konsola","yazdırılacak"
+
+arr = [1,2,3,4,5]
+
+puts arr
+print arr
