f15e869059dd4b027c8c205b010845e3f82720fe
Yasin <yasincoban1998@outlook.com>
2022-12-19 01:08:11 +0300

    Karşılaştırma Operatörleri

---
+++ lesson-14/karsilastirmaOpr.rb
@@ -0,0 +1,27 @@
+a = true
+b = false
+
+
+puts a == b
+##
+puts a != b 
+##
+puts a > b
+##
+puts a >= b
+##
+puts a < b
+##
+puts a <= b
+##
+puts a <=> b
+##
+puts (1..10) === 9
+##
+puts 1.eql?(1)
+##
+nesne1 = 10
+nesne2 = nesne1
+
+puts nesne1.equal?(nesne2)
+##
