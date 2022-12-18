37b194a08983a4101e4e3ce253e2564919b323a8
Yasin <yasincoban1998@outlook.com>
2022-12-19 01:02:04 +0300

    Atama Operatörleri

---
+++ lesson-13/atamaOpr.rb
@@ -0,0 +1,17 @@
+# = : Atama Operatörü
+# Sağındaki değeri solundaki değere atar
+x = 6 #x değişkenine 1 değerini atadık
+
+# += : kendi değerini sağındaki değer ile toplar ve tekrar atama yapar
+puts x += 2
+# -= : kendi değerini sağındaki değer ile çıkartır ve tekrar atama yapar
+puts x -= 2
+# (*)= : kendi değerini sağındaki değer ile çarpar ve tekrar atama yapar
+puts x *= 2
+# /= : kendi değerini sağındaki değer ile böler ve tekrar atama yapar
+puts x /= 2
+# %= : kendi değerini sağındaki değer ile modunu alıp ve tekrar atama yapar
+puts x %= 2
+
+# (**)= : kendi değerini sağındaki değer ile üssünü alıp ve tekrar atama yapar
+puts x **= 2
